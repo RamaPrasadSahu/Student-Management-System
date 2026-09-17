5dba89f4b6d855b4db8e2fa63639155b3462e9d5
Rama Prasad Sahu <ramaprasadsahu008@gmail.com>
2026-09-17 22:53:15 +0530

    feat: implement user model with password hashing and token generation methods

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -33,3 +33,35 @@ const userSchema = new Schema({
     timestamps : true
 })
 
+userSchema.pre("save" ,async function () {
+    if(!this.isModified("password")) return;    
+    this.password = await bcrypt.hash(this.password,10);
+})
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password , this.password)
+}
+userSchema.methods.generateAccesstoken = function (){
+    return jwt.sign ({
+        _id : this._id,
+        fullName : this.fullName,
+        email : this.email
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn : process.env.ACCESS_TOKEN_EXPIRY
+    }
+)
+}
+
+userSchema.methods.generateRefreshtoken = function () {
+    return jwt.sign ({
+        _id : this._id
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn : process.env.REFRESH_TOKEN_EXPIRY
+    }
+ )
+}
+
+export const User = mongoose.model("User",userSchema)
